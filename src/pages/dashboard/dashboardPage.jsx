5aa38c3d3b7f5ac39f4de308cc717cab3819a283
yabuJunn <juancho04reyes@gmail.com>
2025-05-18 15:00:23 -0500

    feat(hotfix): Importadas las fuentes

--- src/pages/dashboard/dashboardPage.jsx
+++ src/pages/dashboard/dashboardPage.jsx
@@ -3,11 +3,22 @@ import './dashboardPage.css'
 import { GlobalNavBar } from '../../components/globalNavBar/globalNavBar'
 import { UpperNavButtons } from '../../components/upperNavButtons/upperNavButtons'
 
+const mockUserName = "Carolina"
+
 export const DashboardPage = () => {
 	return <>
 		<main className='page' id='dashboardPage'>
-			<GlobalNavBar></GlobalNavBar>
-			<UpperNavButtons></UpperNavButtons>
+			{/* <GlobalNavBar></GlobalNavBar>
+			<UpperNavButtons></UpperNavButtons> */}
+
+			<div id='dashboardPagePresentation'>
+				<h1>¡Hola, {mockUserName}!</h1>
+				<p>Bienvenida a tu proyecto <strong>Violet</strong>. !Ve, qué alegría tenerte por acá!</p>
+			</div>
+
+			<div id="dashboardPageCards">
+
+			</div>
 		</main>
 	</>
 }
