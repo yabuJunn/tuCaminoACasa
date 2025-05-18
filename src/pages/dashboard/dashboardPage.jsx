45eba572596500926069f2c2c69c8503482db25a
yabuJunn <juancho04reyes@gmail.com>
2025-05-18 14:41:09 -0500

    feat(hotfix): Creada la estructura de los upperNavButtons

--- src/pages/dashboard/dashboardPage.jsx
+++ src/pages/dashboard/dashboardPage.jsx
@@ -1,12 +1,13 @@
 import './dashboardPage.css'
 
 import { GlobalNavBar } from '../../components/globalNavBar/globalNavBar'
+import { UpperNavButtons } from '../../components/upperNavButtons/upperNavButtons'
 
 export const DashboardPage = () => {
 	return <>
 		<main className='page' id='dashboardPage'>
 			<GlobalNavBar></GlobalNavBar>
-
+			<UpperNavButtons></UpperNavButtons>
 		</main>
 	</>
 }
