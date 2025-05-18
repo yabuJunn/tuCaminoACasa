aaa7317cea13a452e98e1cf8f885a4968cf0581a
Sergio Restrepo Prado <sergio.restrepo@u.icesi.edu.co>
2025-05-18 10:22:41 -0500

    Feat: agrega los logos al componente logosInicio

--- src/components/LogosInicio/logosInicio.jsx
+++ src/components/LogosInicio/logosInicio.jsx
@@ -2,8 +2,8 @@ const LogosInicio = () => {
     return(
         <>
             <div>
-                <img src="" alt="" />
-                <img src="" alt="" />
+                <img src="./assets/Logos/Logo-TuCaminoACasa.svg" alt="Logo_Tu_Camino_A_Casa" />
+                <img src="./assets/Logos/Logo-JaramilloMora.svg" alt="Logo_Jaramillo_Mora" />
             </div>
         </>
     )
