b4ef04f8e369eda793c2a25aa6b53ff22bc4e1e1
JuanVB1 <131167099+juanvelasco1@users.noreply.github.com>
2025-05-18 13:38:24 -0500

    feat:Login

--- src/components/FormLogin/formLogin.jsx
+++ src/components/FormLogin/formLogin.jsx
@@ -1,24 +1,25 @@
+import "./formLogin.css"; 
 const FormLogin = () => {
     return (
         <>
             <div id="loginForm">
-                <h1>¡Bienvenido de nuevo!</h1>
-                <div>
-                    <h2>Iniciar sesión</h2>
-                    <p>Ingresa tus datos para acceder a tu cuenta</p>
-                    <form>
-                        <div>
-                            <label htmlFor="email">Emal</label>
+                <h1 id="login-title">¡Bienvenido de nuevo!</h1>
+                <div id="login-box">
+                    <h2 id="login-heading" className="login-heading">Iniciar sesión</h2>
+                    <p id="login-description">Ingresa tus datos para acceder a tu cuenta</p>
+                    <form id="login-form">
+                        <div id="email-group">
+                            <label id="email" htmlFor="email">Email</label>
                             <input type="email" id="email" name="email" required />
                         </div>
-                        <div>
-                            <label htmlFor="password">Contraseña</label>
+                        <div id="password-group">
+                            <label id="contraseña"htmlFor="password">Contraseña</label>
                             <input type="password" id="password" name="password" required />
-                            <a href="#">¿Olvidaste tu contraseña?</a>
+                            <a href="#" id="forgot-password-link">¿Olvidaste tu contraseña?</a>
                         </div>
-                        <button type="submit">Iniciar sesión</button>
+                        <button type="submit" className="form-button" id="submit-button">Iniciar sesión</button>
                     </form>
-                    <p>¿Aun no tienes una cuenta? <a href="#">Regístrate ahora</a></p>
+                    <p id="signup-paragraph">¿Aun no tienes una cuenta? <a href="#">Regístrate ahora</a></p>
                 </div>
             </div>
         </>
