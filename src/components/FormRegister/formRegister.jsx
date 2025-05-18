cca9f5a6634a7e248f5dd06d163d091a460d18d7
JuanVB1 <131167099+juanvelasco1@users.noreply.github.com>
2025-05-18 14:31:18 -0500

    FixRegister

--- src/components/FormRegister/formRegister.jsx
+++ src/components/FormRegister/formRegister.jsx
@@ -1,27 +1,29 @@
+import "./FormRegister.css"
+
 const FormLogin = () => {
     return(
         <>
             <div id="registerForm">
-                <h1>¡Regístrate ahora!</h1>
-                <div>
-                    <h2>Crear una cuenta</h2>
-                    <p>Ingresa tus datos para crear una cuenta</p>
-                    <form>
-                        <div>
+                <h1 id="register-title">¡Regístrate ahora!</h1>
+                <div id="register-box">
+                    <h2 id="register-heading" className="register-heading">Crear una cuenta</h2>
+                    <p id="register-description">Ingresa tus datos para crear una cuenta</p>
+                    <form id="register-form">
+                        <div id="email-group">
                             <label htmlFor="email">Email</label>
                             <input type="email" id="email" name="email" required />
                         </div>
-                        <div>
+                        <div id="password-group">
                             <label htmlFor="password">Contraseña</label>
                             <input type="password" id="password" name="password" required />
                         </div>
-                        <div>
-                            <label htmlFor="password">Confirmar contraseña</label>
+                        <div id="confirm-password-group">
+                            <label htmlFor="password-confirm">Confirmar contraseña</label>
                             <input type="password" id="password-confirm" name="confirm" required />
                         </div>
-                        <button type="submit">Registrarse</button>
+                        <button type="submit" id="register-button">Registrarse</button>
                     </form>
-                    <p>¿Ya tienes una cuenta? <a href="#">Inicia sesión</a></p>
+                    <p id="login-paragraph">¿Ya tienes una cuenta? <a href="#">Inicia sesión</a></p>
                 </div>
             </div>
         </>
--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,10 +1,12 @@
 import FormRegister from '../../components/FormRegister/formRegister';
 import LogosInicio from '../../components/LogosInicio/logosInicio';
-
+import FondoRegister from '../../assets/FondoLoginRegister/Fondo-LoginRegister.svg';
+import "./register.css";
 const RegisterScreen = () => {
     return (
         <>
             <main className='page' id='registerPage'>
+                <img id="FondoRegister" src={FondoRegister} alt="Fondo-LoginRegister" />
                 <LogosInicio></LogosInicio>
                 <FormRegister></FormRegister>
             </main>
