5f19b3c87f326a840dbee6599ce9819c6f672748
Lina Marcela Landys Samboni <lina_landys@Linaaa.local>
2025-05-19 15:42:05 -0500

    style: estilos para componenten del resumen

--- src/components/propertySummary/propertySummary.jsx
+++ src/components/propertySummary/propertySummary.jsx
@@ -1,35 +1,40 @@
-import "./propertySummary.css"
+import "./propertySummary.css";
+import ProjectCardImage from "../../assets/svg/ProjectCard.svg"; 
 
 const projectMock = {
-    title: "Violet",
-    location: "Sur de Cali",
-    type: "Apartamento",
-    class: "1",
-    state: "Obra Gris",
-    size: "88,00 m2"
-}
+  title: "Violet",
+  location: "Sur de Cali",
+  type: "Apartamento",
+  class: "1",
+  state: "Obra Gris",
+  size: "88,00 m2"
+};
 
 export const PropertySummmary = () => {
-    return <>
-        <div className="propertySummaryContainer">
-            <img id="propertySummaryImageBackground" src="" alt="" />
+  return (
+    <div className="propertySummaryContainer">
+      <img
+        id="propertySummaryImageBackground"
+        src={ProjectCardImage}
+        alt="Vista del proyecto Violet"
+      />
 
-            <div id="propertySummaryTextTitle">
-                <h2>{projectMock.title}</h2>
-                <p>{projectMock.title}</p>
-            </div>
+      <div id="propertySummaryTextTitle">
+        <h2>{projectMock.title}</h2>
+        <p>{projectMock.location}</p>
+      </div>
 
-            <div id="propertySummaryTextDescription">
-                <div>
-                    <h3>{projectMock.type} tipo {projectMock.class}</h3>
-                    <h4>{projectMock.state}</h4>
-                    <p>{projectMock.size}</p>
-                </div>
-
-                <button id="propertySummaryViewMoreButton">
-                    {"Ver más >"}
-                </button>
-            </div>
+      <div id="propertySummaryTextDescription">
+        <div>
+          <h3>{projectMock.type} tipo {projectMock.class}</h3>
+          <h4>{projectMock.state}</h4>
+          <p>{projectMock.size}</p>
         </div>
-    </>
-}
+
+        <button id="propertySummaryViewMoreButton">
+          {"Ver más >"}
+        </button>
+      </div>
+    </div>
+  );
+};
