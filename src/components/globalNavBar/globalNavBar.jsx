3b23fcdab97b5a9933a5211656fb84493ceefdf6
yabuJunn <juancho04reyes@gmail.com>
2025-05-18 19:34:09 -0500

    feat: added styles to the navbar

--- src/components/globalNavBar/globalNavBar.jsx
+++ src/components/globalNavBar/globalNavBar.jsx
@@ -4,6 +4,25 @@ import { ProjectSelector } from "../projectSelector/projectSelector"
 
 import navbarAppLogo from "../../assets/svg/logoAppNavbar.svg"
 
+import dashboardIconWhite from "../../assets/svg/dashboardIconWhite.svg"
+import dashboardIconBlack from "../../assets/svg/dashboardIconBlack.svg"
+
+import myProjectIconWhite from "../../assets/svg/myProjectIconWhite.svg"
+import myProjectIconBlack from "../../assets/svg/myProjectIconBlack.svg"
+
+import whatAmIGoingToDoIconWhite from "../../assets/svg/whatAmIGoingToDoIconWhite.svg"
+import whatAmIGoingToDoIconBlack from "../../assets/svg/whatAmIGoingToDoIconBlack.svg"
+
+import documentsIconWhite from "../../assets/svg/documentsIconWhite.svg"
+import documentsIconBlack from "../../assets/svg/documentsIconBlack.svg"
+
+import bankStatementIconWhite from "../../assets/svg/bankStatementIconWhite.svg"
+import bankStatementIconBlack from "../../assets/svg/bankStatementIconBlack.svg"
+
+import learnIconWhite from "../../assets/svg/learnIconWhite.svg"
+import learnIconBlack from "../../assets/svg/learnIconBlack.svg"
+
+
 export const GlobalNavBar = () => {
     return <>
         <aside id="globalSidebar__nav">
@@ -11,58 +30,44 @@ export const GlobalNavBar = () => {
                 <img src={navbarAppLogo} alt="appLogo" />
             </div>
 
-            {/* <select name="project" id="projectSelection">
-                <option value="violet">
-                    <h2>Violet</h2>
-                    <p>Apartamento</p>
-                </option>
-                <option value="panceNativo">
-                    <h2>Pance Nativo</h2>
-                    <p>Casa</p>
-                </option>
-            </select> */}
-
             <ProjectSelector></ProjectSelector>
 
             <nav className="sidebar__nav">
-                <ul>
-                    <li className="sidebar__nav-item active">
-                        <button className="sidebar__link">
-                            <img src="" alt="dashboardIcon" />
-                            <span>Resumen</span>
-                        </button>
-                    </li>
-                    <li className="sidebar__nav-item">
-                        <button className="sidebar__link">
-                            <img src="" alt="myProjectIcon" />
-                            <span>Mi Proyecto</span>
-                        </button>
-                    </li>
-                    <li className="sidebar__nav-item">
-                        <button className="sidebar__link">
-                            <img src="" alt="whatAmIGoingToDoIcon" />
-                            <span>¿En qué voy?</span>
-                        </button>
-                    </li>
-                    <li className="sidebar__nav-item">
-                        <button className="sidebar__link">
-                            <img src="" alt="documentsIcon" />
-                            <span>Documentos</span>
-                        </button>
-                    </li>
-                    <li className="sidebar__nav-item">
-                        <button className="sidebar__link">
-                            <img src="" alt="bankStatementIcon" />
-                            <span>Estado de cuenta</span>
-                        </button>
-                    </li>
-                    <li className="sidebar__nav-item">
-                        <button className="sidebar__link">
-                            <img src="" alt="learnIcon" />
-                            <span>Aprende</span>
-                        </button>
-                    </li>
-                </ul>
+
+                <button className="sidebar__link focus" id="dashboardButton">
+                    <img src={dashboardIconBlack} alt="dashboardIcon" />
+                    <span>Resumen</span>
+                </button>
+
+
+                <button className="sidebar__link" id="myProjectButtton">
+                    <img src={myProjectIconWhite} alt="myProjectIcon" />
+                    <span>Mi Proyecto</span>
+                </button>
+
+
+                <button className="sidebar__link" id="whatAmIGoingToDoButton">
+                    <img src={whatAmIGoingToDoIconWhite} alt="whatAmIGoingToDoIcon" />
+                    <span>¿En qué voy?</span>
+                </button>
+
+
+                <button className="sidebar__link" id="documentsButton">
+                    <img src={documentsIconWhite} alt="documentsIcon" />
+                    <span>Documentos</span>
+                </button>
+
+
+                <button className="sidebar__link" id="bankStatementButton">
+                    <img src={bankStatementIconWhite} alt="bankStatementIcon" />
+                    <span>Estado cuenta</span>
+                </button>
+
+                <button className="sidebar__link" id="learnButton">
+                    <img src={learnIconWhite} alt="learnIcon" />
+                    <span>Aprende</span>
+                </button>
+
             </nav>
         </aside>
     </>
