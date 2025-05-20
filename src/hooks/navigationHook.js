f67876d036d7268b21eaa08d6f8344c46fd85cb9
yabuJunn <juancho04reyes@gmail.com>
2025-05-19 22:13:48 -0500

    feat(feature): Creado el customHook de navigation con las rutas actuales

---
+++ src/hooks/navigationHook.js
@@ -0,0 +1,27 @@
+import { useNavigate } from "react-router-dom";
+import { useCallback } from "react";
+
+export const NavigationHook = () => {
+    const navigate = useNavigate();
+
+    const navigateToLogin = useCallback(() => navigate("/"), [navigate]);
+    const navigateToRegister = useCallback(() => navigate("/register"), [navigate]);
+    const navigateToDashboard = useCallback(() => navigate("/dashboard"), [navigate]);
+    const navigateToProfile = useCallback(() => navigate("/profile"), [navigate]);
+    const navigateToMyProject = useCallback(() => navigate("/my-project"), [navigate]);
+    const navigateToEnQueVoy = useCallback(() => navigate("/en-que-voy"), [navigate]);
+
+    const handleNavigation = {
+        navigateToLogin,
+        navigateToRegister,
+        navigateToDashboard,
+        navigateToProfile,
+        navigateToMyProject,
+        navigateToEnQueVoy
+    }
+
+    return {
+        handleNavigation
+    }
+}
+
