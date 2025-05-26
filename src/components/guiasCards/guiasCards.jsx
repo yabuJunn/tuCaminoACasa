3c51123d213c487810ed9033fcae3b53588abd59
salazar2356 <isasalazar2356@gmail.com>
2025-05-25 21:32:33 -0500

    Feature:Iconos de guiasCards importados

--- src/components/guiasCards/guiasCards.jsx
+++ src/components/guiasCards/guiasCards.jsx
@@ -1,8 +1,10 @@
+import bancoIcon from "../../assets/guiasCardsIcons/banco.svg"
+
 export const GuiaCards =({ icon, title, description })=>{
     return(
         <div className="GuiaCards">
         <div>
-            <img src={checkIcon} alt="checkIcon" />
+            <img src={bancoIcon} alt="bancoIcon" />
    </div>
 
     </div>
