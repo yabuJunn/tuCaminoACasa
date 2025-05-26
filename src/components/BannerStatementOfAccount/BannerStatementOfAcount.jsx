decd238a49551740824f3e12c337999126b11118
JuanVB1 <131167099+juanvelasco1@users.noreply.github.com>
2025-05-26 16:33:01 -0500

    botones-banner

--- src/components/BannerStatementOfAccount/BannerStatementOfAcount.jsx
+++ src/components/BannerStatementOfAccount/BannerStatementOfAcount.jsx
@@ -1,12 +1,18 @@
+import "./BannerStatementOfAcount.css";
 import { UpperNavButtons } from "../upperNavButtons/upperNavButtons";
+import BannerStatement from "../../assets/banner-StatementOfAccount/Banner-StatementOfAccount.svg";
 
 const BannerStatementOfAccount = () => {
   return (
-    <div id="statementOfAccountBanner">
-        <img src="#" alt="" />
-        <h1>Estado de cuenta</h1>
-        <h2>Resumen</h2>
-        <UpperNavButtons></UpperNavButtons>
+    <div id="bannerStatementContainer">
+      <img id="bannerStatement" src={BannerStatement} alt="Banner-StatementOfAccount" />
+      <div id="statementTextContainer">
+        <h1 id="statementTitle">Estado de cuenta</h1>
+        <h2 id="statementSubtitle">Resumen</h2>
+      </div>
+      <div id="statementButtonsContainer">
+        <UpperNavButtons />
+      </div>
     </div>
   );
 };
