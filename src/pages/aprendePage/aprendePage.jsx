333bc42a11fd5d01a359cb1d8dfc132d702056b3
salazar2356 <isasalazar2356@gmail.com>
2025-05-25 22:19:13 -0500

    Feature: Añadida la NavBar a la Page

--- src/pages/aprendePage/aprendePage.jsx
+++ src/pages/aprendePage/aprendePage.jsx
@@ -1,10 +1,13 @@
 import { AprendeHeader } from "../../components/aprendeHeader/aprendeHeader"
 import { ArticulosSection } from "../../components/articulos/articulos"
 import { CardsGuia } from "../../components/cardsGuia/cardsGuia"
+import { GlobalNavBar } from "../../components/globalNavBar/globalNavBar"
 
 export const AprendePage = () => {
 	return <>
     <section className="aprendePage">
+            <GlobalNavBar></GlobalNavBar>
+    
 
      <div className="headerSection">
     <AprendeHeader></AprendeHeader>
