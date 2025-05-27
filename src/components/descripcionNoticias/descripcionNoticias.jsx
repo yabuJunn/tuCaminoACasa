08f80b6726bfa502ad981c72115caa469ef5d3bc
salazar2356 <isasalazar2356@gmail.com>
2025-05-27 14:15:19 -0500

    Feature: Añadido descripcion a la sesion de noticias

---
+++ src/components/descripcionNoticias/descripcionNoticias.jsx
@@ -0,0 +1,21 @@
+
+
+import { AprendeNoticiasBanner } from "../../components/aprendeNoticiasBanner/aprendeNoticiasBanner"
+
+export const descripcionNoticias = () => {
+    return<>
+
+    <section className="aprendeNoticiasDescripcion">
+
+        <h3 className="fechaAprendeNoticias"> 7 de julio, 2025</h3>
+        <h1 className="pageNoticiasTitle">Lo que debe saber del nuevo subsidio a tasa para viviendas clase media</h1>
+        <h2 className="pageNoticiasSubtitle">Cubre rango entre $ 130 y $ 440 millones. De 100.000 coberturas, 60.000 son para primera vivienda.</h2>
+        <h2 className="pageNoticiasDescripcion">El Gobierno, a través del decreto 1233, expedido este lunes por el Ministerio de Hacienda, asignó 100.000 
+            coberturas de tasa de interés (subsidio a la tasa) para créditos hipotecarios y operaciones de leasing habitacional, otorgados por entidades
+             financieras y cajas de compensación familiar, para compra de vivienda nueva no VIS de hasta 500 salarios mínimos (438 millones de pesos en 2020). 
+             Le puede interesar: Pese a la pandemia, aumenta la venta de vivienda nueva en el país De acuerdo con la norma, que dispone de los recursos del Fondo
+              de Reserva para la Estabilización de la Cartera Hipotecaria (Frech), estas coberturas están disponibles hasta diciembre de 2022 o hasta que se agoten las existencias.</h2>
+
+    </section>
+    </>
+}
