0cd3422a774c9ea9bbe17fc5a20725759acf352a
salazar2356 <isasalazar2356@gmail.com>
2025-05-26 20:57:54 -0500

    Feature:  Estructura de sesion de puntos clave de firma creada

---
+++ src/components/puntosClaveFirma/puntosClaveFirma.jsx
@@ -0,0 +1,13 @@
+import documentoIcon from "../../assets/svgIconsAprende/documentoIcon.svg"
+import revisionIcon from "../../assets/svgIconsAprende/revisionIcon.svg"
+import firmaIcon from "../../assets/svgIconsAprende/firmaIcon.svg"
+
+export const puntosClaveFirma = () => {
+    return<>
+
+    <section className="puntosClaveGeneralCardsFirma">
+        
+      
+    </section>
+    </>
+}
