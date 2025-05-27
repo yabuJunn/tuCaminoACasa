e80333475d301931f47155fc40df8048a20e2000
salazar2356 <isasalazar2356@gmail.com>
2025-05-26 22:26:16 -0500

    Feature: Tutoriales de registro estructura general con info correspondiente

--- src/components/tutorialesAprendeRegistro/tutorialesAprendeRegistro.jsx
+++ src/components/tutorialesAprendeRegistro/tutorialesAprendeRegistro.jsx
@@ -8,18 +8,18 @@ export const TutorialesAprendeRegistro = () => {
         <div  className="tutorialesRegistroCard">
                 <img src={videoBanco} alt="videoBanco" />
                 <h3 className="video">Vídeo</h3>
-                <h3 className="cardTutorialTitle">Paso a paso para firmar la escritura</h3>
-                <p className="cardTutorialDescription">Descubre en minutos cómo es el proceso de firma: qué documentos llevar, qué pagos 
-                    hacer y cómo asegurarte de que tu propiedad quede registrada correctamente, sin complicaciones. </p>
+                <h3 className="cardTutorialTitle">Paso a paso para el registro</h3>
+                <p className="cardTutorialDescription">Conoce paso a paso cómo registrar tu vivienda: qué documentos necesitas, cuánto cuesta y cómo 
+                    asegurarte de que quedes legalmente inscrito como propietario. </p>
                 </div>
                 <button className="verAhora">Ver ahora</button>
 
                  <div  className="tutorialesFirmaCard">
                 <img src={documentoGuiaIcon} alt="documentoGuiaIcon" />
                 <h3 className="documentoGuía">Documento/Guía</h3>
-                <h3 className="cardTutorialTitle">Certificación laboral</h3>
-                <p className="cardTutorialDescription">Aprende qué debe incluir tu certificación laboral, por qué es
-                     clave para tu crédito hipotecario y cómo presentarla correctamente para evitar rechazos.  </p>
+                <h3 className="cardTutorialTitle">Guia de documentos para el registro</h3>
+                <p className="cardTutorialDescription">Escritura, paz y salvo, y pagos — para completar el registro de tu
+                     propiedad sin contratiempos.  </p>
                 </div>
                 <button className="descargarGuia">Descargar Guía</button>
 
