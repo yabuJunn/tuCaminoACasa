4619e94917006ca0c4a6cf471b1be533da0237cc
salazar2356 <isasalazar2356@gmail.com>
2025-05-26 21:08:32 -0500

    Feature:Terminadas las cards de la sesion puntos clave de FirmaPage

--- src/components/puntosClaveFirma/puntosClaveFirma.jsx
+++ src/components/puntosClaveFirma/puntosClaveFirma.jsx
@@ -16,16 +16,15 @@ export const PuntosClaveFirma = () => {
 
         <div  className="puntoClaveFirmaCard">
                         <img src={glassesIcon} alt="glassesIcon" />
-                        <h3 className="cardPuntoTitleFirma">Firma de la escritura</h3>
-                        <p className="cardPuntoDescriptionFirma">Carga el documento con tú firma de la escritura
-                             para que la entrega del inmueble no tenga retrasos. </p>
+                        <h3 className="cardPuntoTitleFirma">Revisión del acuerdo final</h3>
+                        <p className="cardPuntoDescriptionFirma">Repasa las condiciones del contrato y asegúrate de que todo esté correcto. </p>
                         </div>
 
          <div  className="puntoClaveFirmaCard">
                         <img src={homeIcon} alt="homeIcon" />
-                        <h3 className="cardPuntoTitleFirma">Firma de la escritura</h3>
-                        <p className="cardPuntoDescriptionFirma">Carga el documento con tú firma de la escritura
-                             para que la entrega del inmueble no tenga retrasos. </p>
+                        <h3 className="cardPuntoTitleFirma">Registro de propiedad</h3>
+                        <p className="cardPuntoDescriptionFirma">El trámite final: inscribir la escritura en la Oficina de Registro para
+                            quedar como propietario legal. </p>
                         </div>
         
       
