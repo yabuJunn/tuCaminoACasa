461b5bb601fade68c7f674812bf0e56f20473d7d
Lina Marcela Landys Samboni <lina_landys@Linaaa.local>
2025-06-03 12:03:29 -0500

    hotfix: css para page de registro y componentes

--- src/components/aprendeScreen/aprendeRegsitroBanner/aprendeRegistroBanner.jsx
+++ src/components/aprendeScreen/aprendeRegsitroBanner/aprendeRegistroBanner.jsx
@@ -1,23 +1,22 @@
 import iconBack from "../../../assets/svg/aprendeScreen/svgIconsAprende/iconBack.svg"
 import bannerRegistro from "../../../assets/svg/aprendeScreen/ImgBannerAprende/bannerRegistro.svg"
-
+import { UpperNavButtons } from "../../../components/navigation/upperNavButtons/upperNavButtons"
+import "./registroBanner.css"
 
 export const AprendeRegistroBanner = () => {
-    return<>
-
-        <div className="bannerAprendeRegistro">
-        <img src={iconBack} alt="iconBack" />
-
-        <img src={bannerRegistro} alt="bannerRegistro" />
-
-         <div className="breadCrumb">
-        <span className="page">Aprende</span>
-        <span className="separator">&gt;</span>
-        <span className="subPage">Registro de la escritura</span>
-      </div>
-
+  return (
+    <div className="bannerAprendeRegistro" style={{ backgroundImage: `url(${bannerRegistro})` }}>
+      <div className="bannerOverlay">
+        <div className="topBar">
+          <img src={iconBack} alt="Volver" className="backIcon" />
+          <UpperNavButtons />
         </div>
 
-
-    </>
+        <div className="breadcrumbText">
+          <span className="breadcrumb">Aprende &gt;</span>
+          <span className="titulo"> Registro de la escritura</span>
+        </div>
+      </div>
+    </div>
+  )
 }
