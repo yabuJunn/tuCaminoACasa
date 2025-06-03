04b9728c487c661a890dd5d07d97a7df07e5514a
Lina Marcela Landys Samboni <lina_landys@Linaaa.local>
2025-06-03 12:16:21 -0500

    hotfix: css para page y componentes de entrega

--- src/components/aprendeScreen/tutorialesAprendeEntrega/tutorialesAprendeEntrega.jsx
+++ src/components/aprendeScreen/tutorialesAprendeEntrega/tutorialesAprendeEntrega.jsx
@@ -1,18 +1,19 @@
 import videoBanco from "../../../assets/svg/aprendeScreen/svgIconsAprende/videoIcon.svg"
 import documentoGuiaIcon from "../../../assets/svg/aprendeScreen/svgIconsAprende/documentoGuiaIcon.svg"
+import "./tutorialEntrega.css"
 
 export const TutorialesAprendeEntrega = () => {
     return <>
 
-        <section className="tutorialesEntregaGeneralCards">
-            <div className="tutorialesEntregaCard">
+        <section className="tutorialesBancoGeneralCards">
+            <div className="tutorialesBancoCard">
                 <img src={videoBanco} alt="videoBanco" />
                 <h3 className="video">Vídeo</h3>
                 <h3 className="cardTutorialTitle">Paso a paso para recibir tu vivienda</h3>
                 <p className="cardTutorialDescription">Conoce qué revisar al momento de la entrega, cómo firmar el acta
                     y qué hacer si encuentras detalles pendientes.</p>
+                    <button className="verAhora">Ver ahora</button>
             </div>
-            <button className="verAhora">Ver ahora</button>
 
             <div className="tutorialesFirmaCard">
                 <img src={documentoGuiaIcon} alt="documentoGuiaIcon" />
@@ -20,8 +21,8 @@ export const TutorialesAprendeEntrega = () => {
                 <h3 className="cardTutorialTitle">Acta y entrega del inmueble</h3>
                 <p className="cardTutorialDescription">Aprende qué incluye el acta de entrega, tus derechos como comprador
                     y cómo asegurar que todo esté en orden antes de recibir las llaves.</p>
+                    <button className="descargarGuia">Descargar Guía</button>
             </div>
-            <button className="descargarGuia">Descargar Guía</button>
 
 
         </section>
