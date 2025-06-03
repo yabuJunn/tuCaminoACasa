2699b556cfb8d02fe03cfe30179d83824678c9eb
Lina Marcela Landys Samboni <lina_landys@Linaaa.local>
2025-06-03 11:11:17 -0500

    Hotfix: css para la page y componentes de aprendeBanco

--- src/components/aprendeScreen/aprendeBancoBanner/aprendeBancoBanner.jsx
+++ src/components/aprendeScreen/aprendeBancoBanner/aprendeBancoBanner.jsx
@@ -1,22 +1,23 @@
 import "./bancoBanner.css"
-
+import { UpperNavButtons } from "../../../components/navigation/upperNavButtons/upperNavButtons"
 import iconBack from "../../../assets/svg/aprendeScreen/svgIconsAprende/iconBack.svg"
 import bannerImg from "../../../assets/svg/aprendeScreen/ImgBannerAprende/bannerBanco.svg"
 
 export const AprendeBancoBanner = () => {
   return (
-    <div className="bannerAprendeBanco">
+    <div className="bannerAprendeBanco" style={{ backgroundImage: `url(${bannerImg})` }}>
+      <div className="bannerOverlay">
+        <div className="topBar">
+          <img src={iconBack} alt="Volver" className="backIcon" />
+          <UpperNavButtons/>
+        </div>
 
-      <div className="breadCrumb">
-        <span className="page">Aprende</span>
-        <span className="separator">&gt;</span>
-        <span className="subPage">Trámite con el banco</span>
+        <div className="breadcrumbText">
+          <span className="breadcrumb">Aprende &gt;</span>
+          <span className="titulo"> Trámite con el banco</span>
+        </div>
       </div>
-
-      <img src={bannerImg} alt="bannerImg" className="bgBanner" />
-      <img src={iconBack} alt="iconBack" className="backIcon" />
-
-
     </div>
   )
 }
+
--- src/pages/aprende/aprendeBancoPage/aprendeBancoPage.jsx
+++ src/pages/aprende/aprendeBancoPage/aprendeBancoPage.jsx
@@ -3,9 +3,7 @@ import "./aprendeBanco.css"
 import { AprendeBancoBanner } from "../../../components/aprendeScreen/aprendeBancoBanner/aprendeBancoBanner"
 import { PuntosClaveBanco } from "../../../components/aprendeScreen/puntosClaveBanco/puntosClaveBanco"
 import { TutorialesAprendeBanco } from "../../../components/aprendeScreen/tutorialesAprendeBanco/tutorialesAprendeBanco"
-
 import { GlobalNavBar } from "../../../components/navigation/globalNavBar/globalNavBar"
-import { UpperNavButtons } from "../../../components/navigation/upperNavButtons/upperNavButtons"
 
 export const AprendeBancoPage = () => {
     return <>
@@ -16,8 +14,6 @@ export const AprendeBancoPage = () => {
                 <GlobalNavBar></GlobalNavBar>
             </div>
 
-
-
             <div className="bannerAprendeBanco">
                 <AprendeBancoBanner></AprendeBancoBanner>
             </div>
@@ -27,14 +23,12 @@ export const AprendeBancoPage = () => {
                 <h2 className="puntosClave">Puntos clave</h2>
                 <div className="cardsAprendeBanco">
                     <PuntosClaveBanco></PuntosClaveBanco>
-
-                    <h2 className="tutorialesCards">Tutoriales y recursos</h2>
-                    <TutorialesAprendeBanco></TutorialesAprendeBanco>
                 </div>
-
 
             </div>
 
+            <h2 className="tutorialesCards">Tutoriales y recursos</h2>
+            <TutorialesAprendeBanco></TutorialesAprendeBanco>
         </section >
     </>
 }
