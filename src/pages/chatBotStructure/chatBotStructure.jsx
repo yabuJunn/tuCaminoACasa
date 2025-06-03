ded1657180fbc540db9400cc7406b6d0667bd050
salazar2356 <isasalazar2356@gmail.com>
2025-06-03 17:44:59 -0500

    Feature: añadido componentes de botones

--- src/pages/chatBotStructure/chatBotStructure.jsx
+++ src/pages/chatBotStructure/chatBotStructure.jsx
@@ -5,10 +5,10 @@ export const ChatBotStructure = () => {
         <h5 className="date">Hoy 10:30 am</h5>
 
         <div className="mensaje">
-            <h4 className="saludo">Hola, Salome!! </h4>
-            <h1 className="pregunta">¿Cómo puedo ayudarte el día de hoy? </h1>
-            <h2 className="descripcion">Obtén respuestas instantáneas sobre tu proyecto, documentos o comunícate con un asesor. </h2>
-            <h4 className="dateMessage">10:30 am</h4>
+            <h4 className="saludoMensaje">Hola, Salome!! </h4>
+            <h1 className="preguntaMensaje">¿Cómo puedo ayudarte el día de hoy? </h1>
+            <h2 className="descripcionMensaje">Obtén respuestas instantáneas sobre tu proyecto, documentos o comunícate con un asesor. </h2>
+            <h4 className="dateMensaje">10:30 am</h4>
 
             </div>
 
