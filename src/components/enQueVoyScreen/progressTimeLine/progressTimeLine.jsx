f4315430b96f8b0b23d3b33453a45cccc14efe86
Thescientifhic <sebasartu2110@gmail.com>
2025-06-04 19:17:01 -0500

    En que voy style complete

--- src/components/enQueVoyScreen/progressTimeLine/progressTimeLine.jsx
+++ src/components/enQueVoyScreen/progressTimeLine/progressTimeLine.jsx
@@ -1,4 +1,5 @@
 import "../progressTimeLine/progressTimeLine.css";
+import check from "../../../assets/svg/enQueVoyScreen/check.svg"
 
 export const ProgressTimeLine = () => {
     return (
@@ -10,58 +11,59 @@ export const ProgressTimeLine = () => {
 
                 <div className="steps">
                     <div className="generalStructureCheck">
-                        <div className="number check">
-                            <h4>1</h4>
+                        <div className="number verde">
+                            <img src={check} alt="Check icon" />
                         </div>
-                        <div className="seleccionCheck">
-                            <p className="seleccion">Selección del inmueble</p>
+                        <div className="seleccionCheck texto-verde">
+                            <p className="texto-verde">Selección del inmueble</p>
                         </div>
-                    </div>
+                        </div>
 
-                    <div className="generalStructureCheck">
-                        <div className="number check">
-                            <h4>2</h4>
+                        <div className="generalStructureCheck">
+                        <div className="number verde">
+                            <img src={check} alt="Check icon" />
                         </div>
-                        <div className="separacionCheck">
-                            <p className="separacion">Separación del inmueble</p>
+                        <div className="separacionCheck texto-verde">
+                            <p className="texto-verde">Separación del inmueble</p>
                         </div>
-                    </div>
+                        </div>
 
-                    <div className="generalStructureCheck">
-                        <div className="number check">
-                            <h4>3</h4>
+                        <div className="generalStructureCheck">
+                        <div className="number verde">
+                            <img src={check} alt="Check icon" />
                         </div>
-                        <div className="firmaCheck">
-                            <p className="firma">Firma de documentación</p>
+                        <div className="firmaCheck texto-verde">
+                            <p className="texto-verde">Firma de documentación</p>
                         </div>
-                    </div>
+                        </div>
 
-                    <div className="generalStructureCheck">
-                        <div className="number">
+                        <div className="generalStructureCheck">
+                        <div className="number amarillo">
                             <h4>4</h4>
                         </div>
-                        <div className="construccionCheck">
-                            <p className="construccion">Construcción en curso</p>
+                        <div className="construccionCheck texto-amarillo">
+                            <p className="texto-amarillo">Construcción en curso</p>
                         </div>
-                    </div>
+                        </div>
 
-                    <div className="generalStructureCheck">
-                        <div className="number">
+                        <div className="generalStructureCheck">
+                        <div className="number gris">
                             <h4>5</h4>
                         </div>
-                        <div className="entregaCheck">
-                            <p className="entrega">Entrega del inmueble</p>
+                        <div className="entregaCheck texto-gris">
+                            <p className="texto-gris">Entrega del inmueble</p>
                         </div>
-                    </div>
+                        </div>
 
-                    <div className="generalStructureCheck">
-                        <div className="number">
+                        <div className="generalStructureCheck">
+                        <div className="number gris">
                             <h4>6</h4>
                         </div>
-                        <div className="escrituracionCheck">
-                            <p className="escrituración">Escrituración</p>
+                        <div className="escrituracionCheck texto-gris">
+                            <p className="texto-gris">Escrituración</p>
                         </div>
-                    </div>
+                        </div>
+ 
                 </div>
             </section>
         </>
